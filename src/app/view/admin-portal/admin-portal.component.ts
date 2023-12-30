20c13cdaac86169cf085880631fb6472a580d218
DanielB-02 <daniel.d.bor@gmail.com>
2023-12-30 14:30:14 +0100

    Functional admin portal
    User is able to add product inside of the admin portal.

--- src/app/view/admin-portal/admin-portal.component.ts
+++ src/app/view/admin-portal/admin-portal.component.ts
@@ -26,52 +26,23 @@ export class AdminPortalComponent {
     this.signupForm = this.fb.group({
       name: [null, [Validators.required]],
       description: [null, [ Validators.required]],
-      price: [null, [Validators.email, Validators.required]],
+      price: [null, [Validators.required]],
     });
   }
 
-  togglePasswordVisibility() {
-    this.hidePassword = !this.hidePassword;
-  }
-
   onSubmit(): void {
-
-    if (this.signupForm.valid) {
-      const password = this.signupForm.get('password')?.value;
-      const confirmPassword = this.signupForm.get('confirmPassword')?.value;
-
-      if (password !== confirmPassword) {
-        this.snackBar.open('Passwords do not match.', 'Close', { duration: 5000, panelClass: 'error-snackbar' });
-        return;
+    this.isSpinning = true;
+    this.authService.register(this.signupForm.value).subscribe(
+      (response) => {
+        this.isSpinning = false;
+        console.log(response);
+        this.snackBar.open('Product added succesfully!', 'Close', { duration: 5000 });
+        this.router.navigateByUrl("/main-view");
+      },
+      (error) => {
+        this.isSpinning = false;
+        this.snackBar.open('Sign up failed. Please try again.', 'Close', { duration: 5000, panelClass: 'error-snackbar' });
       }
-
-      this.isSpinning = true;
-
-      // Perform the signup logic here
-      // Example: Assuming you have an AuthService with a signup method
-      this.authService.register(this.signupForm.value).subscribe(
-        (response) => {
-          this.isSpinning = false;
-          console.log(response);
-          this.snackBar.open('Sign up successful!', 'Close', { duration: 5000 });
-          // Navigate to the login page or perform any other action
-          this.router.navigateByUrl("/main-view/adminpanel");
-        },
-        (error) => {
-          this.isSpinning = false;
-          this.snackBar.open('Sign up failed. Please try again.', 'Close', { duration: 5000, panelClass: 'error-snackbar' });
-        }
-      );
-    } else {
-      for (const i in this.signupForm.controls) {
-        this.signupForm.controls[i].markAsDirty();
-        this.signupForm.controls[i].updateValueAndValidity();
-      }
-    }
-  }
-
-  public setFictRole(fictRole: boolean) {
-    this.signupForm.get('fictRole').setValue(fictRole);
-    console.log(this.signupForm.get('fictRole'));
+    );
   }
 }
